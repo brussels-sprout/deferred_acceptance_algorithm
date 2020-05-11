d33e6eec84e032c0cdfe7670b0eef531def2560d
Brussels Sprout <34946565+brussels-sprout@users.noreply.github.com>
2020-05-12 00:15:40 +0300

    Add elements.py Remove Man.py and Woman.py Update algorithm.py
    Move Man and Woman classes to elements.py
    Bug fixes in elements.py and algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -2,32 +2,37 @@
 # by brussels_sprout
 
 
-from Woman import Woman
-from Man import Man
+from elements import *
 
 
 # dict (dictionary) contains names (string) as keys and preferences (list where smaller index is higher preference) as values
 def algorithm(women_dict, men_dict):
     create_objects(women_dict, men_dict)
 
-    women = Woman.collection
-    men = Man.collection
+    print(women)
+    print(men)
 
     not_finished = True
 
     while not_finished:
-        if False in [w.is_engaged for w in women]:  # if not all women are engaged
+        if False in [w.is_engaged for w in women.values()]:  # if not all women are engaged
             for w in women.values():
                 if not w.is_engaged and len(w.preferences) != 0:
-                    w.propose(w.preferces[0])
+                    w.propose(w.preferences[0])
         else:
             not_finished = False
 
 
 def create_objects(women_dict, men_dict):
-    for w in women_dict.items():
-        Woman(*w)
+    for i in range(2):
+        for w in women_dict.items():
+            Woman(*w)
 
-    for m in men_dict.items():
-        Man(*m)
+        for m in men_dict.items():
+            Man(*m)
 
+
+women_dict_ = {"1": ["b", "c", "d", "a"], "2": ["a", "d", "b", "c"], "3": ["c", "b", "d", "a"], "4": ["a", "b", "d", "c"]}
+men_dict_ = {"a": ["1", "3", "2", "4"], "b": ["4", "3", "1", "2"], "c": ["2", "1", "3", "4"], "d": ["2", "3", "1", "4"]}
+
+algorithm(women_dict_, men_dict_)
---
+++ elements.py
@@ -0,0 +1,83 @@
+# deferred_acceptance_algorithm
+# by brussels_sprout
+
+
+class Woman:
+    collection = {}
+
+    def __init__(self, name, preferences):
+        self.name = name  # string
+
+        self.preferences = []
+        self.set_preferences(preferences)
+
+        self.engaged_with = None
+        self.is_engaged = bool(self.engaged_with)
+
+        self.update_collection(self)
+
+    def set_preferences(self, preferences):  # parameter is list of strings
+        if men == {}:
+            pass
+        else:
+            for p in preferences:
+                self.preferences.append(men[p])
+
+    @classmethod
+    def update_collection(cls, self):  # takes the class and the object; adds object to collection with its name as the key
+        cls.collection.update({self.name: self})
+
+    def propose(self, man):  # man object that woman proposes to
+        man.be_proposed(self)
+
+    def be_rejected(self, man):  # man object who rejected the woman object
+        self.preferences.remove(man)
+
+    def be_engaged(self, man):  # man object who engaged the woman (could be temporary or permanent engagement)
+        self.engaged_with = man
+
+
+class Man:
+    collection = {}
+
+    def __init__(self, name, preferences):
+        self.name = name  # string
+
+        self.preferences = []
+        self.set_preferences(preferences)
+
+        self.engaged_with = None  # woman object
+        self.is_engaged = bool(self.engaged_with)
+
+        self.update_collection(self)
+
+    def set_preferences(self, preferences):  # parameter is list of strings
+        if women == {}:
+            pass
+        else:
+            for p in preferences:
+                self.preferences.append(women[p])
+
+    @classmethod
+    def update_collection(cls, self):  # takes the class and the object; adds object to collection with its name as the key
+        cls.collection.update({self.name: self})
+
+    def be_proposed(self, woman):  # woman object man got proposed by
+        if not self.is_engaged:
+            self.engage(woman)
+        elif self.preferences.index(woman) < self.preferences.index(self.engaged_with):
+            self.reject(self.engaged_with)
+            self.engage(woman)
+        else:
+            self.reject(woman)
+
+    def reject(self, woman):  # woman object man rejected
+        woman.be_rejected(self)
+
+    def engage(self, woman):  # woman object man engaged (could be temporary or permanent engagement)
+        self.engaged_with = woman
+        woman.be_engaged(self)
+
+
+women = Woman.collection
+men = Man.collection
