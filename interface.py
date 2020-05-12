00ef9cb1035d8a38ad4b13d06a43b46f44297419
Brussels Sprout <34946565+brussels-sprout@users.noreply.github.com>
2020-05-12 17:15:19 +0300

    Update interface.py
    Define intro() and end()

--- interface.py
+++ interface.py
@@ -2,9 +2,27 @@
 # by brussels_sprout
 
 
+def intro():
+    title()
+
+
+def title():
+    print("\033[1m" + "Deferred Acceptance Algorithm" + "\033[0;0m" + "\nby brussels-sprout\n")
+    # weird things make it bold
+
+
 def input_():
     pass
 
 
 def output():
     pass
+
+
+def end(main):  # main is function to run again
+    if input("\nInput any character(s) to run again or simply press ENTER to exit: ") == "":
+        print("\nDone.")
+        exit()
+    else:
+        print("")
+        main()
