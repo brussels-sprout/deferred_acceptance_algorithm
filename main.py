5899818de0ff507edc7f7eb730e37fea6c95fc57
Brussels Sprout <34946565+brussels-sprout@users.noreply.github.com>
2020-05-12 17:19:22 +0300

    Update main.py
    Add file description

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # deferred_acceptance_algorithm
 # by brussels_sprout
 
+# main.py combines the interface and the algorithm
+
 
 from algorithm import algorithm
 from interface import intro, input_, output, end
