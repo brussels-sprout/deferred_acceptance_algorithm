2b4f37a3327de7190bf8c478a66254ac8ad91f1b
Brussels Sprout <34946565+brussels-sprout@users.noreply.github.com>
2020-05-15 15:04:32 +0300

    Update interface.py
    Complete info()

--- interface.py
+++ interface.py
@@ -16,7 +16,12 @@ def title():
 
 
 def info():
-    pass
+    print("Information:")
+    print(" • For an explanation of the algorithm and the problem watch https://youtu.be/Qcv1IqHWAzg by Numberphile.")
+    print(" • The notations used (\"woman\" and \"man\") are according the the Numberphile video.")
+    print(" • So, in this program, the women \"propose\" to the men.")
+    print(" • Note that the inputs and output are case sensitive.")
+    print(" • Also note that this program currently only solves the problem for two sets of elements of equal size.")
 
 
 def input_():
