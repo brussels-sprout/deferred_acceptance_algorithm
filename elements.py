ea8c9b62ae8451fd6ac70eef941732bc0d185c0a
Brussels Sprout <34946565+brussels-sprout@users.noreply.github.com>
2020-05-28 19:34:54 +0300

    Update elements.py
    Reformat

--- elements.py
+++ elements.py
@@ -7,34 +7,43 @@
 class Woman:
     collection = {}  # {object.name: object, ...} (format)
 
-    def __init__(self, name):  # preferences (not self.preferences) is a list of names (strings)
+    def __init__(self, name):
         self.name = name  # string
 
-        self.preferences = []  # list of objects; index 0 is the most preferable
+        # list of objects; index 0 is the most preferable
+        self.preferences = []
 
         self.engaged_with = None  # man object
 
         self.update_collection(self)
 
-    def set_preferences(self, preferences):  # adds man objects to woman's preference list
-        for p in preferences:  # p is a name (string)
+    # preferences (not self.preferences) is a list of names (strings)
+    # adds man objects to woman's preference list
+    def set_preferences(self, preferences):
+        # p is a name (string)
+        for p in preferences:
             self.preferences.append(men[p])
 
     @property
     def is_engaged(self):
         return bool(self.engaged_with)  # bool(None) == False
 
+    # adds object to collection with its name as the key
     @classmethod
-    def update_collection(cls, self):  # adds object to collection with its name as the key
+    def update_collection(cls, self):
         cls.collection.update({self.name: self})
 
-    def propose(self, man):  # man object that woman proposes to
+    # man object that woman proposes to
+    def propose(self, man):
         man.be_proposed(self)
 
-    def be_engaged(self, man):  # man object who engaged the woman (could be temporary or permanent engagement)
+    # man object who engaged the woman
+    # (could be temporary or permanent engagement)
+    def be_engaged(self, man):
         self.engaged_with = man
 
-    def be_rejected(self, man):  # man object who rejected the woman
+    # man object who rejected the woman
+    def be_rejected(self, man):
         self.preferences.remove(man)
 
         self.engaged_with = None
@@ -43,16 +52,19 @@ class Woman:
 class Man:
     collection = {}  # {object.name: object, ...} (format)
 
-    def __init__(self, name):  # preferences (not self.preferences) is a list of names (strings)
+    def __init__(self, name):
         self.name = name  # string
 
-        self.preferences = []  # list of objects; index 0 is the most preferable
+        # list of objects; index 0 is the most preferable
+        self.preferences = []
 
         self.engaged_with = None  # woman object
 
         self.update_collection(self)
 
-    def set_preferences(self, preferences):  # adds woman objects to man's preference list
+    # preferences (not self.preferences) is a list of names (strings)
+    # adds woman objects to man's preference list
+    def set_preferences(self, preferences):
         for p in preferences:
             self.preferences.append(women[p])
 
@@ -60,26 +72,33 @@ class Man:
     def is_engaged(self):
         return bool(self.engaged_with)  # bool(None) == False
 
+    # adds object to collection with its name as the key
     @classmethod
-    def update_collection(cls, self):  # adds object to collection with its name as the key
+    def update_collection(cls, self):
         cls.collection.update({self.name: self})
 
-    def be_proposed(self, woman):  # woman object man got proposed by
-        if not self.is_engaged:  # man not engaged
+    # woman object man got proposed by
+    def be_proposed(self, woman):
+        # if man not engaged then
+        if not self.is_engaged:
             self.engage(woman)
-        # if woman proposing is more preferable than engaged woman then ...
-        elif self.preferences.index(woman) < self.preferences.index(self.engaged_with):
+        # if woman proposing is more preferable than engaged woman then
+        elif self.preferences.index(woman) <\
+                self.preferences.index(self.engaged_with):
             self.reject(self.engaged_with)
             self.engage(woman)
         else:
             self.reject(woman)
 
-    def engage(self, woman):  # woman object man engaged (could be temporary or permanent engagement)
+    # woman object man engaged
+    # (could be temporary or permanent engagement)
+    def engage(self, woman):
         self.engaged_with = woman
 
         woman.be_engaged(self)
 
-    def reject(self, woman):  # woman object man rejected
+    # woman object man rejected
+    def reject(self, woman):
         woman.be_rejected(self)
 
 
