efd5bba34c5603a3106c6e373bfd580dd5b82150
Brussels Sprout <34946565+brussels-sprout@users.noreply.github.com>
2020-05-28 19:34:36 +0300

    Update algorithm.py
    Reformat

--- algorithm.py
+++ algorithm.py
@@ -3,28 +3,32 @@
 
 # algorithm.py contains the pure algorithm
 
-# Regarding the notation ("woman" and "man") used in the code, it is according to
-# the Numberphile video (https://youtu.be/Qcv1IqHWAzg) on this problem.
+# Regarding the notation ("woman" and "man") used in the code,
+# it is according to the Numberphile video
+# (https://youtu.be/Qcv1IqHWAzg) on this problem.
 
 
 from elements import *
 
 
-# dict (dictionary) contains names (string) as keys and preferences (list where smaller index is higher preference) as values
+# input (dictionary) contains names (string) as keys and preferences
+# (list where smaller index is higher preference) as values
 def algorithm(women_input, men_input):
     create_objects(women_input, men_input)
 
     not_finished = True
 
     while not_finished:
-        if False in [woman.is_engaged for woman in women.values()]:  # if not all women are engaged
+        # if not all women are engaged
+        if False in [woman.is_engaged for woman in women.values()]:
             for woman in women.values():
                 if not woman.is_engaged:
                     woman.propose(woman.preferences[0])
         else:
             not_finished = False
 
-    arrangements_with_objects = {}  # {woman: man, ...} (format; woman and man are objects)
+    # {woman: man, ...} (format; woman and man are objects)
+    arrangements_with_objects = {}
 
     for woman in women.values():
         arrangements_with_objects.update({woman: woman.engaged_with})
@@ -37,10 +41,10 @@ def algorithm(women_input, men_input):
 def create_objects(women_input, men_input):
     # create objects without preferences
     for w in women_input.keys():
-        Woman(w)  # w is just the woman's name
+        Woman(w)  # w is just the woman's name (string)
 
     for m in men_input.keys():
-        Man(m)  # m is just the man's name
+        Man(m)  # m is just the man's name (string)
 
     # set preferences of created objects
     for name, woman in women.items():  # name is woman.name (string)
@@ -50,7 +54,8 @@ def create_objects(women_input, men_input):
         man.set_preferences(men_input[name])
 
 
-def objects_to_names(arrangements_with_objects):  # replaces objects in arrangements_with_objects with their names
+# replaces objects in arrangements_with_objects with their names
+def objects_to_names(arrangements_with_objects):
     arrangements_with_names = {}
 
     for arrangement in arrangements_with_objects.items():
@@ -60,8 +65,19 @@ def objects_to_names(arrangements_with_objects):  # replaces objects in arrangem
     return arrangements_with_names
 
 
-# Example inputs and output:
-# women_in_test = {"1": ["b", "c", "d", "a"], "2": ["a", "d", "b", "c"], "3": ["c", "b", "d", "a"], "4": ["a", "b", "d", "c"]}
-# men_in_test = {"a": ["1", "3", "2", "4"], "b": ["4", "3", "1", "2"], "c": ["2", "1", "3", "4"], "d": ["2", "3", "1", "4"]}
+# Control (Test/Example) inputs and output
+# women_input_test = {
+#     "1": ["b", "c", "d", "a"],
+#     "2": ["a", "d", "b", "c"],
+#     "3": ["c", "b", "d", "a"],
+#     "4": ["a", "b", "d", "c"]
+# }
+# men_input_test = {
+#     "a": ["1", "3", "2", "4"],
+#     "b": ["4", "3", "1", "2"],
+#     "c": ["2", "1", "3", "4"],
+#     "d": ["2", "3", "1", "4"]
+# }
 #
-# print(algorithm(women_in_test, men_in_test))
+# print(algorithm(women_input_test, men_input_test))
+# should print {'1': 'c', '2': 'a', '3': 'd', '4': 'b'}
