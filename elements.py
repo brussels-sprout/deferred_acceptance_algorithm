a5eba2a4ac66f78ce1bfed63a97dcfa3e9a4619a
Brussels Sprout <34946565+brussels-sprout@users.noreply.github.com>
2020-07-06 23:25:37 +0300

    Update elements.py
    Remove duplicate code and create Element class

--- elements.py
+++ elements.py
@@ -4,8 +4,8 @@
 # elements.py contains the class definitions and related variables
 
 
-class Woman:
-    collection = {}  # {object.name: object, ...} (format)
+class Element:
+    collection = None  # ignore this, i mean it. :)
 
     def __init__(self, name):
         self.name = name  # string
@@ -13,9 +13,22 @@ class Woman:
         # list of objects; index 0 is the most preferable
         self.preferences = []
 
-        self.engaged_with = None  # man object
+        self.engaged_with = None  # man/woman object
 
         self.update_collection(self)
+
+    @property
+    def is_engaged(self):
+        return bool(self.engaged_with)  # bool(None) is False
+
+    # adds object to collection with its name as the key
+    @classmethod
+    def update_collection(cls, self):
+        cls.collection.update({self.name: self})
+
+
+class Woman(Element):
+    collection = {}  # {object.name: object, ...} (format)
 
     # preferences (not self.preferences) is a list of names (strings)
     # adds man objects to woman's preference list
@@ -23,15 +36,6 @@ class Woman:
         # p is a name (string)
         for p in preferences:
             self.preferences.append(men[p])
-
-    @property
-    def is_engaged(self):
-        return bool(self.engaged_with)  # bool(None) == False
-
-    # adds object to collection with its name as the key
-    @classmethod
-    def update_collection(cls, self):
-        cls.collection.update({self.name: self})
 
     # man object that woman proposes to
     def propose(self, man):
@@ -49,18 +53,8 @@ class Woman:
         self.engaged_with = None
 
 
-class Man:
+class Man(Element):
     collection = {}  # {object.name: object, ...} (format)
-
-    def __init__(self, name):
-        self.name = name  # string
-
-        # list of objects; index 0 is the most preferable
-        self.preferences = []
-
-        self.engaged_with = None  # woman object
-
-        self.update_collection(self)
 
     # preferences (not self.preferences) is a list of names (strings)
     # adds woman objects to man's preference list
@@ -68,22 +62,13 @@ class Man:
         for p in preferences:
             self.preferences.append(women[p])
 
-    @property
-    def is_engaged(self):
-        return bool(self.engaged_with)  # bool(None) == False
-
-    # adds object to collection with its name as the key
-    @classmethod
-    def update_collection(cls, self):
-        cls.collection.update({self.name: self})
-
     # woman object man got proposed by
     def be_proposed(self, woman):
         # if man not engaged then
         if not self.is_engaged:
             self.engage(woman)
         # if woman proposing is more preferable than engaged woman then
-        elif self.preferences.index(woman) <\
+        elif self.preferences.index(woman) < \
                 self.preferences.index(self.engaged_with):
             self.reject(self.engaged_with)
             self.engage(woman)
